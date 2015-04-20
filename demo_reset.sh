9bbaebba4e2de0a3fb8c211130d7887e89412c54
Carl Caum <carl@carlcaum.com>
2015-04-20 15:54:58 -0700

    Update demo_reset script

--- demo_reset.sh
+++ demo_reset.sh
@@ -3,22 +3,19 @@ vagrant snap rollback jenkins.inf.puppetlabs.demo --name demo_start
 vagrant snap rollback wordpress-staging.pdx.puppetlabs.demo --name demo_start
 vagrant snap rollback wordpress.pdx.puppetlabs.demo --name demo_start
 
-cd ~/cloudbees/profile
+cd ~/projects/cloudbees/cloudbees-profile
 git co nginx
-git reset --hard d7d2094
+git reset --hard d47163a
 git push ccaum nginx --force
 git co master
-git reset --hard 3502b10
+git reset --hard 239c066
 git push puppetlabs master --force
+git pull_request -b puppetlabs:master --title "Use nginx intead of apache"
 
-cd ~/cloudbees/site
+cd ~/projects/cloudbees/cloudbees-site
 git co staging
-git reset --hard 2b68ec1
+git reset --hard a836317
 git push puppetlabs staging --force
 git co production
-git reset --hard 7679357
+git reset --hard 0ca6aeb
 git push puppetlabs production --force
-
-cd ~/projects/cloudbees-demo
-JENKINS_IP=`vagrant ssh jenkins.inf.puppetlabs.demo -c 'facter ipaddress_enp0s8'`
-ps aux | grep jnlp | grep -v grep || javaws http://$JENKINS_IP:8080/computer/provisioner/slave-agent.jnlp
