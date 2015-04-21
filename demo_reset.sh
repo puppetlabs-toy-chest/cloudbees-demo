28bb099f4fe255a1856eaeb9545d407c6d291e2d
Carl Caum <carl@carlcaum.com>
2015-04-21 14:45:43 -0700

    Update commits and reset tasks

--- demo_reset.sh
+++ demo_reset.sh
@@ -5,12 +5,11 @@ vagrant snap rollback wordpress.pdx.puppetlabs.demo --name demo_start
 
 cd ~/projects/cloudbees/cloudbees-profile
 git co nginx
-git reset --hard 1802465
+git reset --hard e9cf568
 git push ccaum nginx --force
 git co master
-git reset --hard 7b41567
+git reset --hard 2d17d09
 git push puppetlabs master --force
-#git pull_request -b puppetlabs:master --title "Use nginx intead of apache"
 
 cd ~/projects/cloudbees/cloudbees-site
 git co staging
@@ -19,3 +18,16 @@ git push puppetlabs staging --force
 git co production
 git reset --hard 0ca6aeb
 git push puppetlabs production --force
+
+cd ~/projects/cloudbees-demo
+vagrant ssh -c 'sudo /opt/puppet/bin/r10k deploy environment -p production' master.inf.puppetlabs.demo
+vagrant ssh -c 'sudo service pe-puppetserver restart' master.inf.puppetlabs.demo
+vagrant ssh -c 'sudo /opt/puppet/bin/puppet agent -t' jenkins.inf.puppetlabs.demo
+vagrant ssh -c 'sudo /opt/puppet/bin/puppet agent -t' master.inf.puppetlabs.demo
+vagrant ssh -c 'sudo service network restart' master.inf.puppetlabs.demo
+vagrant ssh -c 'sudo service network restart' jenkins.inf.puppetlabs.demo
+vagrant ssh -c 'sudo service network restart' wordpress.pdx.puppetlabs.demo
+vagrant ssh -c 'sudo service network restart' wordpress-staging.pdx.puppetlabs.demo
+vagrant ssh -c 'sudo service network restart' wordpress-staging.pdx.puppetlabs.demo
+vagrant ssh -c 'sudo service pe-mcollective restart' wordpress.pdx.puppetlabs.demo
+vagrant ssh -c 'sudo service pe-mcollective restart' wordpress-staging.pdx.puppetlabs.demo
